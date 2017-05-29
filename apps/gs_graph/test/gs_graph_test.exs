9f37b098add04ab25a783a706108cc987679b043
dexterlb <dexterlb@qtrp.org>
2017-05-30 00:05:36 +0300

    dummy test

--- apps/gs_graph/test/gs_graph_test.exs
+++ apps/gs_graph/test/gs_graph_test.exs
@@ -5,4 +5,19 @@ defmodule GSGraphTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "can add root object" do
+    node = GSGraph.make_node(%{foo: 'bar'})
+
+    GSGraph.update!([
+      {:adopt, node, nil}
+    ])
+
+    got = GSGraph.get(node.id)
+    assert got.parent == nil
+    assert got.pseudo_parents == %{}
+    assert got.children == %{}
+    assert got.pseudo_children == %{}
+    assert got.data == %{foo: 'bar'}
+  end
 end
