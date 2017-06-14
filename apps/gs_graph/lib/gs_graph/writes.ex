4bec81c86b9a4b98e5dbda5540ea85dcabbc1306
dexterlb <dexterlb@qtrp.org>
2017-06-14 15:13:09 +0300

    use transaction read inside write operations

--- apps/gs_graph/lib/gs_graph/writes.ex
+++ apps/gs_graph/lib/gs_graph/writes.ex
@@ -12,7 +12,7 @@ defmodule GSGraph.Writes do
   def run({:adopt, child, {new_parent_id, new_label}}) do
     clear_from_parent(child)
 
-    new_parent = GSGraph.get(new_parent_id)
+    new_parent = Node.read(new_parent_id)
     
     child       |> Node.set_parent(new_parent_id, new_label)
     new_parent  |> Node.add_child(child.id, new_label)    
@@ -21,7 +21,7 @@ defmodule GSGraph.Writes do
   end
 
   def run({:attach, pseudo_child, {pseudo_parent_id, label}}) do
-    pseudo_parent = GSGraph.get(pseudo_parent_id)
+    pseudo_parent = Node.read(pseudo_parent_id)
 
     pseudo_child    |> Node.add_pseudo_parent(pseudo_parent_id, label)
     pseudo_parent   |> Node.add_pseudo_child(pseudo_child.id, label)
@@ -30,7 +30,7 @@ defmodule GSGraph.Writes do
   end
 
   def run({:detach, pseudo_child, {pseudo_parent_id, label}}) do
-    pseudo_parent = GSGraph.get(pseudo_parent_id)
+    pseudo_parent = Node.read(pseudo_parent_id)
     
     pseudo_child |> Node.del_pseudo_parent(pseudo_parent_id, label)
     pseudo_parent |> Node.del_pseudo_child(pseudo_child.id, label)
@@ -41,7 +41,7 @@ defmodule GSGraph.Writes do
   defp clear_from_parent(child) do
     case child.parent do
       {parent_id, label} ->
-        GSGraph.get(parent_id)
+        Node.read(parent_id)
           |> Node.del_child(child.id, label)
       nil -> nil
     end
