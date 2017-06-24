43bb1f9f4b00905856b0e78836316a5f28928fac
dexterlb <dexterlb@qtrp.org>
2017-06-24 12:08:25 +0300

    register action

--- apps/gs_server/lib/gs_server/session.ex
+++ apps/gs_server/lib/gs_server/session.ex
@@ -10,19 +10,42 @@ defmodule GsServer.Session do
     {:noreply, new_state}
   end
 
+  def handle_info({:nodes_changed, nodes}, state = {client, _, _}) do
+    write(client, Poison.encode!(format_nodes(nodes)))
+    {:noreply, state}
+  end
+
   defp handle_json(data, state) do
     %{"action" => action_name, "args" => args} = data
 
     action(action_name, args, state)
   end
 
-  defp action(name, args, state = {_client, action_spec, _node_id}) do
-    apply(action_spec, :run_action, [name, args])
+  defp action(name, args, {client, action_spec, data}) do
+    {:ok, new_data} = apply(
+      action_spec,
+      :run_action, 
+      [name, Map.put(args, "data", data)]
+    )
 
-    state
+    {client, action_spec, new_data}
   end
 
   defp write(client, data) do
     :gen_tcp.send(client, data)
+  end
+
+  defp format_nodes(nodes) do
+    Enum.map(nodes, &format_node/1)
+  end
+
+  defp format_node(node) do
+    %{
+      "data" => node |> GsGraph.data,
+      "children" => node |> GsGraph.children,
+      "pseudo_children" => node |> GsGraph.pseudo_children,
+      "ref" => node.ref,
+      "id" => node.id
+    }
   end
 end
--- apps/sample_game/lib/sample_game/actions.ex
+++ apps/sample_game/lib/sample_game/actions.ex
@@ -1,11 +1,50 @@
 defmodule SampleGame.Actions do
   use GsServer.ActionSpec
 
-  action "register", [foo, bar] do
-    IO.inspect {"foooooo", foo, bar}
+  action "register", [name, data] do
+    nil = data
+
+    field = case GsGraph.get_by_name("field") do
+      nil ->
+        new_field = GsGraph.make_node(%{"type" => "field"})
+        GsGraph.update!([
+          {:set_name, new_field.id, "field"}
+        ])
+        
+        new_field.id
+      old_field -> old_field
+    end
+
+    player = case GsGraph.get_by_name("player " <> name) do
+      nil ->
+        new_player = GsGraph.make_node(%{
+          "type" => "player",
+          "name" => name
+        })
+
+        GsGraph.update!([
+          {:adopt, field, "contains", new_player.id}
+        ])
+
+        new_player.id
+
+      old_player -> old_player
+    end
+          
+    {:ok, %{player_id: player}}
   end
 
-  action "print", [text] do
+  action "print", [text, data] do
     IO.puts ["it was requested that I print ", text]
+
+    {:ok, data}
+  end
+
+  action "id", [data] do
+    %{player_id: id} = data
+
+    IO.inspect {:id, id}
+
+    {:ok, data}
   end
 end
